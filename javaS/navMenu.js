aea53e7fcd577c266d5e434a682d0e43e6b8e3a0
Milos Mirkovic <35512001+milosmirkovic484484@users.noreply.github.com>
2018-03-23 16:19:41 +0100

    Add files via upload

---
+++ javaS/navMenu.js
@@ -0,0 +1,51 @@
+function anim(targetedDiv){
+    var targetedLetter = $("#"+targetedDiv+" .rotate").text();
+
+    var asciiValue = targetedLetter.charCodeAt(0);
+
+    var pointer = 65;
+
+    ChangeChar();
+
+
+    function ChangeChar() {
+
+        if(pointer <= asciiValue){
+            $("#"+targetedDiv+" .rotate").text(String.fromCharCode(pointer));
+
+            pointer++;
+            setTimeout(ChangeChar,20);
+        }
+        else {
+            $(this).stop;
+        }
+
+    }
+
+}
+<!--menu-->
+
+$(document).ready(function () {
+    $("#openMenu").click(function () {
+        $("#main").fadeIn(200);
+        $("#openMenu").animate({left:"-10%"});
+        $("#closeMenu").animate({left:"93%"});
+        $(".menu").animate({left:"-1%"});
+
+
+    });
+
+    $("#closeMenu").click(function () {
+        $("#main").fadeOut(200);
+        $("#openMenu").animate({left:"0%"});
+        $("#closeMenu").animate({left:"100%"});
+        $(".menu").animate({left:"-50%"});
+
+
+    })
+
+});
+
+
+
+
